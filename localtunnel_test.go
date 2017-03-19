b4dea4bbdea102d9a11b9568aca894cf87307949
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-03-18 23:52:38 -0700

    Improve tests

--- localtunnel_test.go
+++ localtunnel_test.go
@@ -29,7 +29,7 @@ func TestLocalTunnel(t *testing.T) {
 	defer server.Close()
 
 	t.Log("setting up LocalTunnel")
-	lt, err := New(port, "", Options{Log: testLog{t}, MaxConnections: 1})
+	lt, err := New(port, "", Options{Log: testLog{t}, MaxConnections: 2})
 	if err != nil {
 		t.Fatal("failed to create LocalTunnel, error: ", err)
 	}
@@ -37,23 +37,28 @@ func TestLocalTunnel(t *testing.T) {
 	// Sleep for 3s giving the server time to register
 	time.Sleep(3 * time.Second)
 
-	t.Log("sending test request to:", lt.URL())
-	res, err := http.Get(lt.URL())
-	if err != nil {
-		t.Fatal("failed to send GET request through tunnel, error: ", err)
-	}
-	defer res.Body.Close()
+	// Let's make 3 requests for good measure
+	for i := 0; i < 3; i++ {
+		t.Log("sending test request to:", lt.URL())
+		var res *http.Response
+		res, err = http.Get(lt.URL())
+		if err != nil {
+			t.Fatal("failed to send GET request through tunnel, error: ", err)
+		}
+		defer res.Body.Close()
 
-	if res.StatusCode != http.StatusOK {
-		t.Error("expected 200 ok, got status: ", res.StatusCode)
-	}
+		if res.StatusCode != http.StatusOK {
+			t.Error("expected 200 ok, got status: ", res.StatusCode)
+		}
 
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Fatal("failed to read response from tunnel, error: ", err)
-	}
-	if string(data) != "hello-world" {
-		t.Error("unexpected response, data: ", string(data))
+		var data []byte
+		data, err = ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Fatal("failed to read response from tunnel, error: ", err)
+		}
+		if string(data) != "hello-world" {
+			t.Error("unexpected response, data: ", string(data))
+		}
 	}
 
 	t.Log("closing LocalTunnel")
